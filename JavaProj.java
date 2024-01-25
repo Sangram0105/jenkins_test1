e52e6cc5db81f063c0dccecbff779563a85ba2bd
-m <-m>
2024-01-25 23:26:06 +0530

    Cancel the number change

--- JavaProj.java
+++ JavaProj.java
@@ -5,17 +5,9 @@ public class JavaProj {
     
     public static void main(String[] args) {
         System.out.println("The New Change is Here !!!");
-      Scanner sc =new Scanner(System.in);
+     
         System.out.println("The New Project");
-        System.out.println("Enter the Number :-");
-        int a=sc.nextInt();
-        
-        if(a%2==0){
-            System.out.println("Even Number!!!");
-        }
-        else{
-            System.out.println("Odd NUmber");
-        }
-        sc.close();
+       
+       
     }
 }
