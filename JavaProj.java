1c78feb92bd7032c3325ea9242f42b98bc2c2dc5
-m <-m>
2024-01-25 23:14:06 +0530

    The new Commit

--- JavaProj.java
+++ JavaProj.java
@@ -1,6 +1,7 @@
 public class JavaProj {
     
     public static void main(String[] args) {
+        System.out.println("The New Change is Here !!!");
         System.out.println("The New Project");
     }
 }
